dcfd1a485f6e71fa6eb6f1c30d470e36e018a822
Ralf Ebert <info@ralfebert.de>
2009-01-12 23:37:23 +0100

    JFace Dialoge

--- de.rcpbuch.addressbook/src/de/rcpbuch/addressbook/AdressListViewPart.java
+++ de.rcpbuch.addressbook/src/de/rcpbuch/addressbook/AdressListViewPart.java
@@ -1,17 +1,23 @@
 package de.rcpbuch.addressbook;
 
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.layout.TableColumnLayout;
 import org.eclipse.jface.viewers.ArrayContentProvider;
 import org.eclipse.jface.viewers.CellEditor;
 import org.eclipse.jface.viewers.CellLabelProvider;
 import org.eclipse.jface.viewers.ColumnWeightData;
+import org.eclipse.jface.viewers.DialogCellEditor;
 import org.eclipse.jface.viewers.EditingSupport;
+import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.jface.viewers.TableViewerColumn;
 import org.eclipse.jface.viewers.TextCellEditor;
 import org.eclipse.jface.viewers.ViewerCell;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.ui.dialogs.ListDialog;
 import org.eclipse.ui.part.ViewPart;
 
 import de.rcpbuch.addressbook.data.Address;
@@ -101,6 +107,54 @@ public class AdressListViewPart extends ViewPart {
 
 		});
 		tableLayout.setColumnData(colZipCity.getColumn(), new ColumnWeightData(25));
+
+		colZipCity.setEditingSupport(new EditingSupport(tableViewer) {
+
+			@Override
+			protected boolean canEdit(Object element) {
+				return true;
+			}
+
+			@Override
+			protected CellEditor getCellEditor(Object element) {
+				return new DialogCellEditor(tableViewer.getTable()) {
+
+					@Override
+					protected Object openDialogBox(Control cellEditorWindow) {
+						ListDialog listDialog = new ListDialog(cellEditorWindow.getShell());
+						listDialog.setTitle("Stadt auswählen");
+						listDialog.setMessage("Bitte wählen Sie eine Stadt aus:");
+						listDialog.setContentProvider(new ArrayContentProvider());
+						listDialog.setLabelProvider(new LabelProvider());
+						listDialog.setInput(AddressbookServices.getAddressService().getAllCities());
+						if (listDialog.open() == Dialog.OK && listDialog.getResult().length > 0) {
+							return listDialog.getResult()[0];
+						} else {
+							MessageDialog.openError(cellEditorWindow.getShell(), "Fehler",
+									"Bitte wählen Sie eine Stadt aus!");
+							return null;
+						}
+					}
+
+				};
+			}
+
+			@Override
+			protected Object getValue(Object element) {
+				Address address = ((Address) element);
+				return address.getZip() + " " + address.getCity();
+			}
+
+			@Override
+			protected void setValue(Object element, Object value) {
+				Address address = (Address) element;
+				if (value != null && !value.equals(address.getZip() + " " + address.getCity())) {
+					address.setCity(String.valueOf(value));
+					tableViewer.refresh();
+				}
+			}
+
+		});
 
 		tableViewer.setContentProvider(new ArrayContentProvider());
 		tableViewer.setInput(AddressbookServices.getAddressService().getAllAddresses());
