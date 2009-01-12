98b2fa9ca1e5f26b6e170900b1b8d27199e2f130
Ralf Ebert <info@ralfebert.de>
2009-01-12 20:32:41 +0100

    JFace TableViewer II

--- de.rcpbuch.addressbook/src/de/rcpbuch/addressbook/AdressListViewPart.java
+++ de.rcpbuch.addressbook/src/de/rcpbuch/addressbook/AdressListViewPart.java
@@ -1,9 +1,14 @@
 package de.rcpbuch.addressbook;
 
+import org.eclipse.jface.layout.TableColumnLayout;
 import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.CellEditor;
 import org.eclipse.jface.viewers.CellLabelProvider;
+import org.eclipse.jface.viewers.ColumnWeightData;
+import org.eclipse.jface.viewers.EditingSupport;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.jface.viewers.TableViewerColumn;
+import org.eclipse.jface.viewers.TextCellEditor;
 import org.eclipse.jface.viewers.ViewerCell;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
@@ -23,7 +28,10 @@ public class AdressListViewPart extends ViewPart {
 	@Override
 	public void createPartControl(Composite parent) {
 
-		TableViewer tableViewer = new TableViewer(parent);
+		TableColumnLayout tableLayout = new TableColumnLayout();
+		parent.setLayout(tableLayout);
+
+		final TableViewer tableViewer = new TableViewer(parent);
 		tableViewer.getTable().setHeaderVisible(true);
 		tableViewer.getTable().setLinesVisible(true);
 
@@ -36,6 +44,32 @@ public class AdressListViewPart extends ViewPart {
 			public void update(ViewerCell cell) {
 				Address address = (Address) cell.getElement();
 				cell.setText(address.getName());
+			}
+
+		});
+		tableLayout.setColumnData(colName.getColumn(), new ColumnWeightData(50));
+
+		colName.setEditingSupport(new EditingSupport(tableViewer) {
+
+			@Override
+			protected boolean canEdit(Object element) {
+				return true;
+			}
+
+			@Override
+			protected CellEditor getCellEditor(Object element) {
+				return new TextCellEditor(tableViewer.getTable());
+			}
+
+			@Override
+			protected Object getValue(Object element) {
+				return ((Address) element).getName();
+			}
+
+			@Override
+			protected void setValue(Object element, Object value) {
+				((Address) element).setName(String.valueOf(value));
+				tableViewer.refresh();
 			}
 
 		});
@@ -52,6 +86,7 @@ public class AdressListViewPart extends ViewPart {
 			}
 
 		});
+		tableLayout.setColumnData(colStreet.getColumn(), new ColumnWeightData(25));
 
 		TableViewerColumn colZipCity = new TableViewerColumn(tableViewer, SWT.NONE);
 		colZipCity.getColumn().setText("PLZ/Ort");
@@ -65,6 +100,7 @@ public class AdressListViewPart extends ViewPart {
 			}
 
 		});
+		tableLayout.setColumnData(colZipCity.getColumn(), new ColumnWeightData(25));
 
 		tableViewer.setContentProvider(new ArrayContentProvider());
 		tableViewer.setInput(AddressbookServices.getAddressService().getAllAddresses());
