63655a8047eeb71d6bf46e4667e70a23e1f37ddb
Ralf Ebert <info@ralfebert.de>
2009-04-11 00:55:37 +0200

    Korrekturen fuer Beispiel SWTBot-Test

--- plugins/de.rcpbuch.addressbook.tests/src/de/rcpbuch/addressbook/AddressBookTests.java
+++ plugins/de.rcpbuch.addressbook.tests/src/de/rcpbuch/addressbook/AddressBookTests.java
@@ -1,70 +1,113 @@
 package de.rcpbuch.addressbook;
 
 import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.*;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swtbot.eclipse.finder.SWTWorkbenchBot;
 import org.eclipse.swtbot.eclipse.finder.finders.CommandFinder;
 import org.eclipse.swtbot.eclipse.finder.widgets.SWTBotEditor;
 import org.eclipse.swtbot.swt.finder.SWTBot;
+import org.eclipse.swtbot.swt.finder.finders.UIThreadRunnable;
+import org.eclipse.swtbot.swt.finder.results.VoidResult;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotList;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotMenu;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotTable;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotTableItem;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotText;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotTree;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.WorkbenchException;
+import org.junit.Before;
 import org.junit.Test;
 
 public class AddressBookTests {
 
 	private final SWTWorkbenchBot bot = new SWTWorkbenchBot();
 
-	@Test
-	public void testNew() throws Exception {
-		bot.menu("File").menu("New").click();
-		bot.shell("New").close();
+	/**
+	 * Vor der Ausfuehrung jeder Testmethode: ggf. offene Fenster schließen,
+	 * alle Editoren schliessen, aktuelle Perspektive zuruecksetzen,
+	 * Standard-Perspektive aktivieren, diese auch zuruecksetzen
+	 */
+	@Before
+	public void setup() {
+		UIThreadRunnable.syncExec(new VoidResult() {
+			public void run() {
+				try {
+					IWorkbench wb = PlatformUI.getWorkbench();
+					IWorkbenchWindow ww = wb.getActiveWorkbenchWindow();
+					Shell activeShell = Display.getDefault().getActiveShell();
+					if (activeShell != ww.getShell()) {
+						activeShell.close();
+					}
+					ww.getActivePage().closeAllEditors(false);
+					ww.getActivePage().resetPerspective();
+					wb.showPerspective(wb.getPerspectiveRegistry()
+							.getDefaultPerspective(), ww);
+					ww.getActivePage().resetPerspective();
+				} catch (WorkbenchException e) {
+					throw new RuntimeException(e);
+				}
+			}
+		});
 	}
 
 	@Test
-	public void testPerspectiveSwitch() throws Exception {
-		for (int i = 0; i < 10; i++) {
-			bot.perspectiveByLabel("Visitenkarten").activate();
-			bot.perspectiveByLabel("Adressen").activate();
-		}
+	public void testAdresseAnlegen() throws Exception {
+		bot.menu("Datei").menu("Neu").click();
+		bot.shell("Neu");
+		SWTBotTree tree = bot.tree();
+		tree.select("Neue Adresse");
+		bot.button("Weiter >").click();
+		bot.text().setText("Otto Muster\nMusterstr. 12\n01234 Musterhausen");
+		bot.button("Fertig stellen").click();
+
+		assertNotNull("Angelegte Adresse nicht in Adressliste", bot
+				.viewByTitle("Adressen").bot().table().getTableItem(
+						"Otto Muster"));
 	}
 
 	@Test
-	public void testEditAddress() throws Exception {
-
-		// slow down swtbot
-		// System.setProperty("org.eclipse.swtbot.playback.delay", "100");
-
+	public void testAddresseEditieren() throws Exception {
 		SWTBotTable table = bot.viewByTitle("Adressen").bot().table();
 		table.select("Heike Winkler", "Marion Graf");
 		table.contextMenu("Adresse öffnen").click();
-
-		assertEquals(2, bot.editors().size());
+		assertEquals("Zwei Editoren muessen goeffnet sein", 2, bot.editors()
+				.size());
 
 		SWTBotEditor editor = bot.activeEditor();
 		assertEquals("Heike Winkler", editor.getTitle());
-		assertFalse(editor.isDirty());
+		assertFalse("Editor unbearbeitet nicht dirty", editor.isDirty());
 		SWTBot editorContents = editor.bot();
 		SWTBotText text = editorContents.textWithLabel("Name:");
-		assertTrue(text.isActive());
-		text.setText("TESTNAME");
-		editorContents.textWithLabel("Straße:").setText("Street");
-		editorContents.textWithLabel("PLZ/Ort:").setText("123");
+		assertTrue("Standard-Eingabefokus Editor auf Namensfeld", text
+				.isActive());
+		text.setText("Heike Muster");
+		editorContents.textWithLabel("Straße:").setText("Musterstrasse");
+		editorContents.textWithLabel("PLZ/Ort:").setText("01234 Musterort");
 
-		assertTrue(editor.isDirty());
-		// editor.save();
-
-		new CommandFinder().findCommand(equalTo("Save")).get(0).click();
-
-		assertFalse(editor.isDirty());
+		assertTrue("Editor nach Aenderung dirty", editor.isDirty());
+		new CommandFinder().findCommand(equalTo("Speichern")).get(0).click();
+		assertFalse("Editor nach dem Speichern nicht dirty", editor.isDirty());
 		editor.close();
-
-		bot.viewByTitle("Adressen").bot().table().getTableItem("TESTNAME");
-
+		assertNotNull("Aenderung nicht sichtbar", bot.viewByTitle("Adressen")
+				.bot().table().getTableItem("Heike Muster"));
 		bot.editorByTitle("Marion Graf").close();
+	}
 
+	@Test
+	public void testVisitenkarten() throws Exception {
+		bot.perspectiveByLabel("Visitenkarten").activate();
+		assertTrue("Visitenkarten-View nicht aktiv", bot.viewByTitle(
+				"Visitenkarten").isActive());
 	}
 
 }
--- plugins/de.rcpbuch.addressbook/src/de/rcpbuch/SomeNewWizard.java
+++ plugins/de.rcpbuch.addressbook/src/de/rcpbuch/SomeNewWizard.java
@@ -1,20 +1,38 @@
 package de.rcpbuch;
 
+import org.apache.commons.lang.StringUtils;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.ui.INewWizard;
 import org.eclipse.ui.IWorkbench;
 
+import de.rcpbuch.addressbook.entities.Address;
+import de.rcpbuch.addressbook.services.AddressbookServices;
+
 public class SomeNewWizard extends Wizard implements INewWizard {
 
+	private FreeTextWizardPage textWizardPage;
+
 	public SomeNewWizard() {
-		addPage(new FirstWizardPage());
-		addPage(new SecondWizardPage());
+		textWizardPage = new FreeTextWizardPage();
+		addPage(textWizardPage);
 	}
 
 	@Override
 	public boolean performFinish() {
-		// true zurückgeben wenn Wizard erfolgreich abgeschlossen wurde
+		String address = textWizardPage.getText();
+
+		// This is way too simple!
+		String[] parts = StringUtils.split(address, "\r\n");
+		String name = parts.length >= 1 ? parts[0] : "";
+		String street = parts.length >= 2 ? parts[1] : "";
+		String zipCity = parts.length >= 3 ? parts[2] : "";
+		String[] zipCityParts = StringUtils.split(zipCity, ' ');
+		String zip = zipCityParts.length >= 1 ? zipCityParts[0] : "";
+		String city = zipCityParts.length >= 2 ? zipCityParts[1] : "";
+
+		AddressbookServices.getAddressService().saveAddress(new Address(null, name, street, zip, city, null));
+
 		return true;
 	}
 
