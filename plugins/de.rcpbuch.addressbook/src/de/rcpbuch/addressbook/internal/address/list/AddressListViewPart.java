176c763a2c7eaccbf92e1f9759f2bbf6d3b6b7ff
Ralf Ebert <info@ralfebert.de>
2009-07-31 18:19:18 +0200

    address list bound using properties

--- plugins/de.rcpbuch.addressbook/src/de/rcpbuch/addressbook/internal/address/list/AddressListViewPart.java
+++ plugins/de.rcpbuch.addressbook/src/de/rcpbuch/addressbook/internal/address/list/AddressListViewPart.java
@@ -6,16 +6,13 @@ import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.layout.TableColumnLayout;
 import org.eclipse.jface.viewers.ArrayContentProvider;
-import org.eclipse.jface.viewers.CellEditor;
-import org.eclipse.jface.viewers.CellLabelProvider;
 import org.eclipse.jface.viewers.ColumnPixelData;
 import org.eclipse.jface.viewers.ColumnWeightData;
 import org.eclipse.jface.viewers.DialogCellEditor;
-import org.eclipse.jface.viewers.EditingSupport;
 import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.jface.viewers.TableViewerColumn;
-import org.eclipse.jface.viewers.ViewerCell;
+import org.eclipse.jface.viewers.TextCellEditor;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
@@ -24,7 +21,8 @@ import org.eclipse.ui.IWorkbenchActionConstants;
 import org.eclipse.ui.dialogs.ListDialog;
 import org.eclipse.ui.part.ViewPart;
 
-import de.rcpbuch.addressbook.entities.Address;
+import de.ralfebert.rcputils.properties.PropertyCellLabelProvider;
+import de.ralfebert.rcputils.properties.PropertyEditingSupport;
 import de.rcpbuch.addressbook.services.IAddressChangeListener;
 import de.rcpbuch.addressbook.services.IAddressService;
 
@@ -60,101 +58,55 @@ public class AddressListViewPart extends ViewPart {
 
 		TableViewerColumn colName = new TableViewerColumn(tableViewer, SWT.NONE);
 		colName.getColumn().setText("Name");
-		colName.setLabelProvider(new CellLabelProvider() {
 
-			@Override
-			public void update(ViewerCell cell) {
-				Address address = (Address) cell.getElement();
-				cell.setText(address.getName());
-			}
-
-		});
-		tableLayout.setColumnData(colName.getColumn(), new ColumnWeightData(50));
+		colName.setLabelProvider(new PropertyCellLabelProvider("name"));
+		tableLayout.setColumnData(colName.getColumn(), new ColumnWeightData(40));
 
 		TableViewerColumn colStreet = new TableViewerColumn(tableViewer, SWT.NONE);
 		colStreet.getColumn().setText("Straße");
-		colStreet.setLabelProvider(new CellLabelProvider() {
+		colStreet.setLabelProvider(new PropertyCellLabelProvider("street"));
+		colStreet.setEditingSupport(new PropertyEditingSupport(tableViewer, "street", new TextCellEditor(table)));
 
-			@Override
-			public void update(ViewerCell cell) {
-				Address address = (Address) cell.getElement();
-				cell.setText(address.getStreet());
-			}
-
-		});
-		tableLayout.setColumnData(colStreet.getColumn(), new ColumnWeightData(25));
+		tableLayout.setColumnData(colStreet.getColumn(), new ColumnWeightData(20));
 
 		TableViewerColumn colZip = new TableViewerColumn(tableViewer, SWT.NONE);
 		colZip.getColumn().setText("PLZ");
-		colZip.setLabelProvider(new CellLabelProvider() {
+		colZip.setLabelProvider(new PropertyCellLabelProvider("zip"));
+		colZip.setEditingSupport(new PropertyEditingSupport(tableViewer, "zip", new TextCellEditor(table)));
 
-			@Override
-			public void update(ViewerCell cell) {
-				Address address = (Address) cell.getElement();
-				cell.setText(address.getZip());
-			}
-
-		});
 		tableLayout.setColumnData(colZip.getColumn(), new ColumnPixelData(80));
 
 		TableViewerColumn colCity = new TableViewerColumn(tableViewer, SWT.NONE);
 		colCity.getColumn().setText("Ort");
-		colCity.setLabelProvider(new CellLabelProvider() {
+		colCity.setLabelProvider(new PropertyCellLabelProvider("city"));
+		tableLayout.setColumnData(colCity.getColumn(), new ColumnWeightData(20));
+
+		TableViewerColumn colCountry = new TableViewerColumn(tableViewer, SWT.NONE);
+		colCountry.getColumn().setText("Land");
+		colCountry.setLabelProvider(new PropertyCellLabelProvider("country.name"));
+		tableLayout.setColumnData(colCountry.getColumn(), new ColumnWeightData(20));
+
+		final DialogCellEditor cityCellEditor = new DialogCellEditor(table) {
 
 			@Override
-			public void update(ViewerCell cell) {
-				Address address = (Address) cell.getElement();
-				cell.setText(address.getCity());
+			protected Object openDialogBox(Control cellEditorWindow) {
+				ListDialog listDialog = new ListDialog(cellEditorWindow.getShell());
+				listDialog.setTitle("Stadt auswählen");
+				listDialog.setMessage("Bitte wählen Sie eine Stadt aus:");
+				listDialog.setContentProvider(new ArrayContentProvider());
+				listDialog.setLabelProvider(new LabelProvider());
+				listDialog.setInput(addressService.getAllCities());
+				if (listDialog.open() == Dialog.OK && listDialog.getResult().length > 0) {
+					return listDialog.getResult()[0];
+				} else {
+					MessageDialog.openError(cellEditorWindow.getShell(), "Fehler", "Bitte wählen Sie eine Stadt aus!");
+					return null;
+				}
 			}
 
-		});
-		tableLayout.setColumnData(colCity.getColumn(), new ColumnWeightData(25));
+		};
 
-		colCity.setEditingSupport(new EditingSupport(tableViewer) {
-
-			@Override
-			protected boolean canEdit(Object element) {
-				return true;
-			}
-
-			@Override
-			protected CellEditor getCellEditor(Object element) {
-				return new DialogCellEditor(table) {
-
-					@Override
-					protected Object openDialogBox(Control cellEditorWindow) {
-						ListDialog listDialog = new ListDialog(cellEditorWindow.getShell());
-						listDialog.setTitle("Stadt auswählen");
-						listDialog.setMessage("Bitte wählen Sie eine Stadt aus:");
-						listDialog.setContentProvider(new ArrayContentProvider());
-						listDialog.setLabelProvider(new LabelProvider());
-						listDialog.setInput(addressService.getAllCities());
-						if (listDialog.open() == Dialog.OK && listDialog.getResult().length > 0) {
-							return listDialog.getResult()[0];
-						} else {
-							MessageDialog.openError(cellEditorWindow.getShell(), "Fehler",
-									"Bitte wählen Sie eine Stadt aus!");
-							return null;
-						}
-					}
-
-				};
-			}
-
-			@Override
-			protected Object getValue(Object element) {
-				Address address = ((Address) element);
-				return address.getCity();
-			}
-
-			@Override
-			protected void setValue(Object element, Object value) {
-				Address address = (Address) element;
-				address.setCity(String.valueOf(value));
-				tableViewer.refresh(element);
-			}
-
-		});
+		colCity.setEditingSupport(new PropertyEditingSupport(tableViewer, "city", cityCellEditor));
 
 		tableViewer.setContentProvider(new ArrayContentProvider());
 
