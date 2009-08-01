f6157ad5b1a3ef2e1e88889a1eaa9ecd3e90d8f1
Ralf Ebert <info@ralfebert.de>
2009-08-01 02:33:43 +0200

    formatters restructured, readable value removed

--- plugins/de.ralfebert.rcpsnippets/src/de/ralfebert/rcpsnippets/internal/snippet01tableproperties/Snippet01TableViewerBuilder.java
+++ plugins/de.ralfebert.rcpsnippets/src/de/ralfebert/rcpsnippets/internal/snippet01tableproperties/Snippet01TableViewerBuilder.java
@@ -14,6 +14,7 @@ import org.eclipse.ui.part.ViewPart;
 import de.ralfebert.rcputils.builder.table.ColumnBuilder;
 import de.ralfebert.rcputils.builder.table.ICellFormatter;
 import de.ralfebert.rcputils.builder.table.TableViewerBuilder;
+import de.ralfebert.rcputils.builder.table.format.Formatter;
 import de.ralfebert.rcputils.properties.BaseValue;
 import de.ralfebert.rcputils.random.RandomData;
 
@@ -40,19 +41,20 @@ public class Snippet01TableViewerBuilder extends ViewPart {
 
 			public void formatCell(ViewerCell cell, Object value) {
 				int population = (Integer) value;
-				if (population > 5000000) {
-					cell.setForeground(cell.getControl().getDisplay().getSystemColor(SWT.COLOR_RED));
-				}
+				int color = (population > 5000000) ? SWT.COLOR_RED : SWT.COLOR_BLACK;
+				cell.setForeground(cell.getControl().getDisplay().getSystemColor(color));
 			}
 
 		});
-		population.format(new DecimalFormat("#,##0"));
+		population.format(Formatter.forInt(new DecimalFormat("#,##0")));
+		population.makeEditable(Formatter.forInt());
 		population.build();
 
 		ColumnBuilder area = t.createColumn("Fläche");
 		area.bindToProperty("stats.areaKm2");
 		area.alignRight();
-		area.format(new DecimalFormat("0.00 km²"));
+		area.format(Formatter.forDouble(new DecimalFormat("0.00 km²")));
+		area.makeEditable(Formatter.forDouble(new DecimalFormat("0.00")));
 		area.build();
 
 		ColumnBuilder density = t.createColumn("Ew./km²");
@@ -64,14 +66,15 @@ public class Snippet01TableViewerBuilder extends ViewPart {
 			}
 
 		});
-		density.format(new DecimalFormat("0.000"));
+		density.format(Formatter.forDouble(new DecimalFormat("0.0")));
 		density.alignRight();
 		density.build();
 
 		ColumnBuilder foundingDate = t.createColumn("Gründung am");
 		foundingDate.bindToProperty("foundingDate");
 		foundingDate.setPixelWidth(100);
-		foundingDate.format(SimpleDateFormat.getDateInstance(SimpleDateFormat.MEDIUM));
+		foundingDate.format(Formatter.forDate(SimpleDateFormat.getDateInstance(SimpleDateFormat.MEDIUM)));
+		foundingDate.makeEditable();
 		foundingDate.build();
 
 		tableViewer = t.build(createSomeData());
@@ -81,7 +84,7 @@ public class Snippet01TableViewerBuilder extends ViewPart {
 	private List<City> createSomeData() {
 		List<City> data = new ArrayList<City>();
 		RandomData randomData = new RandomData();
-		for (int i = 0; i < 5000; i++) {
+		for (int i = 0; i < 2000; i++) {
 			CityStats stats = new CityStats(randomData.someNumber(10000, 10000000), randomData.someNumber(100d, 800d));
 			data.add(new City(randomData.someCity(), randomData.someDate(1200, 1600), stats));
 			randomData.newData();
