b5eb2d257fa933bda03ee289d9b00850ae338756
Ralf Ebert <info@ralfebert.de>
2010-02-21 15:27:12 +0100

    AddressDeleteHandler log message

--- plugins/com.example.addressbook/src/com/example/addressbook/internal/ui/AddressDeleteHandler.java
+++ plugins/com.example.addressbook/src/com/example/addressbook/internal/ui/AddressDeleteHandler.java
@@ -5,6 +5,7 @@ import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.riena.core.wire.InjectService;
 import org.eclipse.ui.handlers.HandlerUtil;
+import org.osgi.service.log.LogService;
 
 import com.example.addressbook.entities.Address;
 import com.example.addressbook.services.IAddressService;
@@ -18,10 +19,12 @@ import de.ralfebert.rcputils.wired.WiredHandler;
 public class AddressDeleteHandler extends WiredHandler {
 
 	private IAddressService addressService;
+	private LogService log;
 
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		ISelection selection = HandlerUtil.getCurrentSelection(event);
 		for (Address address : SelectionUtils.getIterable(selection, Address.class)) {
+			log.log(LogService.LOG_INFO, "Deleting address: " + address); //$NON-NLS-1$
 			addressService.deleteAddress(address.getId());
 		}
 		return null;
@@ -39,6 +42,15 @@ public class AddressDeleteHandler extends WiredHandler {
 
 	public void unbindAddressService(IAddressService addressService) {
 		this.addressService = null;
+	}
+
+	@InjectService
+	public void bindLog(LogService log) {
+		this.log = log;
+	}
+
+	public void unbindLog(LogService log) {
+		this.log = null;
 	}
 
 }
