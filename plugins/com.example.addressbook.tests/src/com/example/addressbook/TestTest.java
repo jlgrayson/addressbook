c49aff07d01964fd16bc61f1e0689b08e9a42bf1
Ralf Ebert <info@ralfebert.de>
2010-02-21 03:10:48 +0100

    added a junit test fragment for trying junit buckminster execution

---
+++ plugins/com.example.addressbook.tests/src/com/example/addressbook/TestTest.java
@@ -0,0 +1,14 @@
+package com.example.addressbook;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestTest {
+
+	@Test
+	public void test() {
+		//fail("test");
+	}
+	
+}
