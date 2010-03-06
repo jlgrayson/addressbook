c987b272647fdac348ad7fa41f19aca1db320193
Ralf Ebert <info@ralfebert.de>
2010-03-06 10:03:16 +0100

    enabled save and restore

--- plugins/com.example.addressbook/src/com/example/addressbook/internal/application/ApplicationWorkbenchAdvisor.java
+++ plugins/com.example.addressbook/src/com/example/addressbook/internal/application/ApplicationWorkbenchAdvisor.java
@@ -1,5 +1,6 @@
 package com.example.addressbook.internal.application;
 
+import org.eclipse.ui.application.IWorkbenchConfigurer;
 import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
 import org.eclipse.ui.application.WorkbenchAdvisor;
 import org.eclipse.ui.application.WorkbenchWindowAdvisor;
@@ -8,11 +9,20 @@ import com.example.addressbook.AddressBook;
 
 public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
 
+	@Override
 	public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
 		return new ApplicationWorkbenchWindowAdvisor(configurer);
 	}
 
+	@Override
 	public String getInitialWindowPerspectiveId() {
 		return AddressBook.PERSPECTIVE_ADDRESS;
 	}
+
+	@Override
+	public void initialize(IWorkbenchConfigurer configurer) {
+		super.initialize(configurer);
+		configurer.setSaveAndRestore(true);
+	}
+
 }
